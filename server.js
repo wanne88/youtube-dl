869cc61af354e00276b99cd62167bd49ec67276a
wanne88 <125958804+wanne88@users.noreply.github.com>
2023-04-22 12:28:16 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+const app = express();
+
+app.get('/download', (req, res) => {
+  const url = req.query.videoUrl;
+  res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  ytdl(url, {
+    format: 'mp4'
+  }).on('data', (chunk) => {
+    res.write(chunk);
+  }).on('end', () => {
+    res.end();
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
